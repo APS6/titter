fa29a753676b895fced87881bc58342c109ac7c2
APS <anirudhapsah@gmail.com>
2024-08-30 16:42:19 +0530

    redirect all to anirudha.pro

--- next.config.js
+++ next.config.js
@@ -1,21 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "lh3.googleusercontent.com"
-            },
-            {
-                protocol: "https",
-                hostname: "uploadthing.com"
-            },
-            {
-                protocol: "https",
-                hostname: "utfs.io"
-            }
-        ],
-    }
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/:x*",
+        destination: "https://anirudha.pro",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
